2dfb83c81e7386c6b58b8abb1a4a93c4c331da79
Rodrigo Muñoz Villegas <rodrigomunozv4@gmail.com>
2025-10-06 23:35:12 -0300

    Entidad Inscripcion

---
+++ Grupo4-SGULP/src/Conexion/Inscripcion.java
@@ -0,0 +1,60 @@
+
+package Conexion;
+
+public class Inscripcion {
+    private int idInscripto;
+    private double nota;
+    private Alumno alumno;
+    private Materia materia;
+    private int añoCursada;
+
+    public Inscripcion(double nota, Alumno alumno, Materia materia, int añoCursada) {
+        this.nota = nota;
+        this.alumno = alumno;
+        this.materia = materia;
+        this.añoCursada = añoCursada;
+    }
+
+    public int getIdInscripto() {
+        return idInscripto;
+    }
+
+    public void setIdInscripto(int idInscripto) {
+        this.idInscripto = idInscripto;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public int getAñoCursada() {
+        return añoCursada;
+    }
+
+    public void setAñoCursada(int añoCursada) {
+        this.añoCursada = añoCursada;
+    }
+    
+    
+}
+
